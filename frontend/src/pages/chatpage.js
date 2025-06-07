f702a850c01f84b1e2d54adc49b112c8ffbc9f35
Hricha Mehra <hrichamehra11@gmail.com>
2025-06-07 14:42:39 +0530

    Render chats

--- frontend/src/pages/chatpage.js
+++ frontend/src/pages/chatpage.js
@@ -1,9 +1,21 @@
-import React from 'react'
-
-const chatpage = () => {
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+const Chatpage = () => {
+const [chats, setChats] = useState([]);
+    const fetchChats = async () => {
+        const {data}= await axios.get("/api/chat");
+        setChats(data);
+    };
+    useEffect(() => {
+        fetchChats();
+    }, []);
   return (
-    <div>chatpage</div>
-  )
-}
+    <div>
+        {chats.map((chat)=>(
+            <div key={chat._id}>{chat.chatName}</div>
+        ))}
+    </div>
+  );
+};
 
-export default chatpage
+export default Chatpage
