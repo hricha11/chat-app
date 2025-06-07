f1c3db810bbf1dc6ce89a1b0b223b8ca95eea73d
Hricha Mehra <hrichamehra11@gmail.com>
2025-06-07 15:22:33 +0530

    fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import { Route } from 'react-router-dom';
-import homepage from './pages/homepage';
-import chatpage from './pages/chatpage';
+import Homepage from './pages/homepage';
+import Chatpage from './pages/chatpage';
 
 function App() {
   return (
     <div className="App">
-      <Route path="/" component={homepage} exact/>
-      <Route path="/chats" component={chatpage} />
+      <Route path="/" component={Homepage} exact/>
+      <Route path="/chats" component={Chatpage} />
     </div>
   );
 }
