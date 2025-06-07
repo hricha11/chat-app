6d25b3cc6872b1abe8f6d94f1e5fb0d3f0c16a23
Hricha Mehra <hrichamehra11@gmail.com>
2025-06-07 14:11:17 +0530

    Home and chatpage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import { Route } from 'react-router-dom';
+import homepage from './pages/homepage';
+import chatpage from './pages/chatpage';
 
 function App() {
   return (
     <div className="App">
-      Hie
+      <Route path="/" component={homepage} exact/>
+      <Route path="/chats" component={chatpage} />
     </div>
   );
 }
---
+++ frontend/src/pages/chatpage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const chatpage = () => {
+  return (
+    <div>chatpage</div>
+  )
+}
+
+export default chatpage
